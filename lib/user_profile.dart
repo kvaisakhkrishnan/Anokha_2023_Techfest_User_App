cb5715e94af46f81740fa6db56e87fc0c2c7458d
MITHUN-MENON-M <mithunmadhuchittur@gmail.com>
2023-03-08 16:26:49 +0530

    User Profile Commit by Mithun

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -66,17 +66,126 @@ class _userProfState extends State<userProf> {
                   alignment: Alignment.center,
                   children: [
                     Container(
-                      alignment: Alignment.centerLeft,
-                      child: themeChangeIcon(notifyParent: refresh),
-                    ),
-                    Container(
                       height: MediaQuery.of(context).size.height * 0.8,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(20.0)),
                         color: islight ? Color(0xFF002845) : Color(0xFFFFFFFC),
                       ),
                       // color: Colors.black,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              child: Image.asset(
+                                  'Images/anokha_circle.png'),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
+                              child: Text(
+                                "PASSPORT",
+                                style: TextStyle(
+                                    fontSize: 30.0,
+                                    color: islight
+                                        ? Color(0xFFFFFFFC)
+                                        : Color(0xFF002845),
+                                    fontFamily: 'Roboto'),
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height*0,
+                              width: MediaQuery.of(context).size.width * 0.35,
+                              child: Container(
+                                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.004),
+                                alignment: Alignment.topCenter,
+                                child: Divider(
+                                  color: islight
+                                      ? Color(0xFFFFFFFC)
+                                      : Color(0xFF002845),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
+                              child: Text(
+                                "Name",
+                                style: TextStyle(
+                                    fontSize: 35.0,
+                                    color: islight
+                                        ? Color(0xFFFFFFFC)
+                                        : Color(0xFF002845),
+                                    fontFamily: "Roboto"),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.0),
+                              child: Text(
+                                "name@gmail.com",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: islight
+                                        ? Color(0xFFFFFFFC)
+                                        : Color(0xFF002845),
+                                    fontFamily: "Roboto"),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
+                              child: Text(
+                                "Account Balance",
+                                style: TextStyle(
+                                    fontSize: 25.0,
+                                    color: islight
+                                        ? Color(0xFFFFFFFC)
+                                        : Color(0xFF002845),
+                                    fontFamily: "Roboto"),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.0),
+                              child: Text.rich(
+                                TextSpan(
+                                    text: "₹",
+                                    style: (TextStyle(
+                                        fontSize: 35.0,
+                                        color: islight
+                                            ? Color(0xFFFFFFFC)
+                                            : Color(0xFF002845),
+                                        fontFamily: "Roboto")),
+                                    children: <InlineSpan>[
+                                      TextSpan(
+                                          text: "0.0",
+                                          style: TextStyle(
+                                              fontSize: 27,
+                                              color: Color(0xFFFF7F11)))
+                                    ]),
+                              ),
+                            ),
+                            Container(
+                              height: MediaQuery.of(context).size.height * 0.27,
+                              child: GestureDetector(
+                                onTap: () {
+                                  _showImageDialog(context);
+                                },
+                                child: Hero(
+                                  tag: 'imageTag',
+                                  child: Container(
+                                    child: Image.network(
+                                        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+                                        color: islight
+                                            ? Color(0xFFFFFFFC)
+                                            : Color(0xFF002845)),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
                     ),
                     Positioned(
                       top: MediaQuery.of(context).size.height * 0,
@@ -107,136 +216,6 @@ class _userProfState extends State<userProf> {
                             shape: BoxShape.circle),
                       ),
                     ),
-                    Positioned(
-                      top: MediaQuery.of(context).size.height * 0.12,
-                      child: Container(
-                        child: SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.65,
-                          child: Column(
-                            children: [
-                              // Text("hey"), for logo
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                  child: Image.asset(
-                                      'Images/anokha_circle.png'),
-                                ),
-                              ),
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height * 0.028,
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "PASSPORT",
-                                  style: TextStyle(
-                                      fontSize: 30.0,
-                                      color: islight
-                                          ? Color(0xFFFFFFFC)
-                                          : Color(0xFF002845),
-                                      fontFamily: 'Roboto'),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 0,
-                                child: SizedBox(
-                                  height: MediaQuery.of(context).size.height*0,
-                                  width: MediaQuery.of(context).size.width * 0.35,
-                                  child: Container(
-                                    alignment: Alignment.topCenter,
-                                    child: Divider(
-                                      color: islight
-                                          ? Color(0xFFFFFFFC)
-                                          : Color(0xFF002845),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Padding(
-                                  padding: EdgeInsets.only(top: 20.0),
-                                  child: Text(
-                                    "Name",
-                                    style: TextStyle(
-                                        fontSize: 35.0,
-                                        color: islight
-                                            ? Color(0xFFFFFFFC)
-                                            : Color(0xFF002845),
-                                        fontFamily: "Roboto"),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "name@gmail.com",
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      color: islight
-                                          ? Color(0xFFFFFFFC)
-                                          : Color(0xFF002845),
-                                      fontFamily: "Roboto"),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 20.0),
-                                child: Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    "Account Balance",
-                                    style: TextStyle(
-                                        fontSize: 25.0,
-                                        color: islight
-                                            ? Color(0xFFFFFFFC)
-                                            : Color(0xFF002845),
-                                        fontFamily: "Roboto"),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Text.rich(
-                                  TextSpan(
-                                      text: "₹",
-                                      style: (TextStyle(
-                                          fontSize: 35.0,
-                                          color: islight
-                                              ? Color(0xFFFFFFFC)
-                                              : Color(0xFF002845),
-                                          fontFamily: "Roboto")),
-                                      children: <InlineSpan>[
-                                        TextSpan(
-                                            text: "0.0",
-                                            style: TextStyle(
-                                                fontSize: 27,
-                                                color: Color(0xFFFF7F11)))
-                                      ]),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 4,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    _showImageDialog(context);
-                                  },
-                                  child: Hero(
-                                    tag: 'imageTag',
-                                    child: Container(
-                                      child: Image.network(
-                                          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
-                                          color: islight
-                                              ? Color(0xFFFFFFFC)
-                                              : Color(0xFF002845)),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
                   ],
                 ),
               ),
