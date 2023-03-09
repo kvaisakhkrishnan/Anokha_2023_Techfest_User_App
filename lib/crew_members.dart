8c5dacb43d6964667a1b3833134e29a3f9ff43f8
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-03-09 11:37:58 +0530

    Crew members page edited

--- lib/crew_members.dart
+++ lib/crew_members.dart
@@ -6,8 +6,6 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'Loading_Screens/events_loading.dart';
-
 class GetCrew extends StatefulWidget {
   const GetCrew({Key? key}) : super(key: key);
 
@@ -35,9 +33,7 @@ class _GetCrewState extends State<GetCrew> {
               if (ss.hasData) {
                 return CrewMembers(list: ss.data);
               } else {
-                return Center(
-                  child: Events_Loading_screen(),
-                );
+                return CircularProgressIndicator();
               }
             }));
   }
@@ -68,178 +64,196 @@ class _CrewMembersState extends State<CrewMembers> {
 
   Color selected_color = HexColor("#002845");
   int selected_index = 1;
-
   int numbers = 1;
   @override
   Widget build(BuildContext context) {
     final List<String> items =
-        List.generate(crew_list.length, (index) => crew_list[index]["role"]);
+    List.generate(crew_list.length, (index) => crew_list[index]["role"]);
     final List<int> a = List.generate(crew_list.length, (index) => index);
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Container(
           child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          toolbarHeight: 0,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: IconButton(
-              onPressed: () {},
-              splashRadius: 20,
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: HexColor("#FFFFFC"),
-              )),
-
-
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: 120,
-              child: GridView.count(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 0,
-                  childAspectRatio: 2 / 6,
-                  children: items
-                      .map((item) => Padding(
-                            padding: EdgeInsets.fromLTRB(8, 15, 8, 8),
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                side: BorderSide(color: Colors.white),
-                                primary:
-                                    (items.indexOf(item) + 1 == selected_index)
-                                        ? HexColor("#FFFFFC")
-                                        : HexColor("#002845"),
-                                shape: StadiumBorder(),
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  numbers = crew_list[items.indexOf(item)]
-                                          ["crews"]
-                                      .length;
-                                  selected_index = items.indexOf(item) + 1;
-                                });
-                              },
-                              child: Text(
-                                item,
-                                style: GoogleFonts.dmSans(
-                                    fontSize: 15,
-                                    color: (items.indexOf(item) + 1 ==
-                                            selected_index)
-                                        ? HexColor("#002845")
-                                        : HexColor("#FFFFFC")),
-                              ),
-                            ),
-                          ))
-                      .toList()),
+            backgroundColor: HexColor("#002845"),
+            appBar: AppBar(
+              backgroundColor: HexColor("#002845"),
+              elevation: 0,
+              leading: IconButton(
+                  onPressed: () {},
+                  splashRadius: 20,
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: HexColor("#FFFFFC"),
+                  )),
+              title: Text("Crew Members",
+                  style: GoogleFonts.dmSans(color: Colors.white)),
+              centerTitle: true,
             ),
-            Expanded(
-              flex: 4,
-              child: AnimationLimiter(
-                child: Container(
-                  child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                      ),
-                      itemCount: numbers,
-                      padding: EdgeInsets.only(
-                          left: 20, right: 10, top: 10, bottom: 10),
-                      itemBuilder: (context, index) {
-                        return AnimationConfiguration.staggeredGrid(
-                          position: index,
-                          duration: const Duration(milliseconds: 500),
-                          columnCount: 2,
-                          child: ScaleAnimation(
-                            child: FadeInAnimation(
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 10),
-                                child: Stack(
-                                  // ignore: prefer_const_literals_to_create_immutables
-                                  children: [
-                                    Padding(
-                                      padding:
+            body: Column(
+              children: [
+                Container(
+                  height: 120,
+                  child: GridView.count(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 0,
+                      childAspectRatio: 2 / 6,
+                      children: items
+                          .map((item) => Padding(
+                        padding: EdgeInsets.fromLTRB(8, 15, 8, 8),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            side: BorderSide(color: Colors.white),
+                            primary:
+                            (items.indexOf(item) + 1 == selected_index)
+                                ? HexColor("#FFFFFC")
+                                : HexColor("#002845"),
+                            shape: StadiumBorder(),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              numbers = crew_list[items.indexOf(item)]
+                              ["crews"]
+                                  .length;
+                              selected_index = items.indexOf(item) + 1;
+                            });
+                          },
+                          child: Text(
+                            item,
+                            maxLines: 2,
+                            style: GoogleFonts.dmSans(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: (items.indexOf(item) + 1 ==
+                                    selected_index)
+                                    ? HexColor("#002845")
+                                    : HexColor("#FFFFFC")),
+                          ),
+                        ),
+                      ))
+                          .toList()),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: AnimationLimiter(
+                    child: Container(
+                      child: GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemCount: (selected_index == 1)
+                              ? crew_list[0]["crews"].length
+                              : numbers,
+                          padding: EdgeInsets.only(
+                              left: 20, right: 10, top: 10, bottom: 10),
+                          itemBuilder: (context, index) {
+                            return AnimationConfiguration.staggeredGrid(
+                              position: index,
+                              duration: const Duration(milliseconds: 500),
+                              columnCount: 2,
+                              child: ScaleAnimation(
+                                child: FadeInAnimation(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: Stack(
+                                      // ignore: prefer_const_literals_to_create_immutables
+                                      children: [
+                                        Padding(
+                                          padding:
                                           EdgeInsets.only(top: circleRadius),
-                                      child: Card(
-                                        shape: RoundedRectangleBorder(
-                                            side:
+                                          child: Card(
+                                            shape: RoundedRectangleBorder(
+                                                side:
                                                 BorderSide(color: Colors.white),
-                                            borderRadius:
+                                                borderRadius:
                                                 BorderRadius.circular(10)),
-                                        child: SizedBox(
-                                          height: MediaQuery.of(context)
+                                            child: SizedBox(
+                                              height: MediaQuery.of(context)
                                                   .size
                                                   .height *
-                                              0.3,
-                                          width: MediaQuery.of(context)
+                                                  0.3,
+                                              width: MediaQuery.of(context)
                                                   .size
                                                   .width *
-                                              0.4,
-                                          child: Center(
-                                            child: Column(children: [
-                                              SizedBox(
-                                                height: 30,
+                                                  0.4,
+                                              child: Center(
+                                                child: Padding(
+                                                  padding:
+                                                  const EdgeInsets.all(3.0),
+                                                  child: Column(children: [
+                                                    SizedBox(
+                                                      height: 30,
+                                                    ),
+                                                    Text(
+                                                      crew_list[selected_index - 1]
+                                                      ["crews"][index]["name"],
+                                                      maxLines: 5,
+                                                      style: GoogleFonts.dmSans(
+                                                          fontSize: 20,
+                                                          fontWeight:
+                                                          FontWeight.bold),
+                                                    ),
+                                                    Padding(
+                                                      padding:
+                                                      const EdgeInsets.only(
+                                                          top: 5),
+                                                      child: Text(
+                                                        crew_list[selected_index -
+                                                            1]["crews"][index]
+                                                        ["department"],
+                                                        style: TextStyle(
+                                                            fontSize: 15,
+                                                            color: Colors.orange),
+                                                      ),
+                                                    )
+                                                  ]),
+                                                ),
                                               ),
-                                              Text(
-                                                "Sharath S R",
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                              Text(
-                                                "CSE",
-                                                style: TextStyle(
-                                                    color: Colors.orange),
-                                              )
-                                            ]),
+                                            ),
+                                            color: HexColor("#FFFFFC"),
+                                            elevation: 10,
                                           ),
                                         ),
-                                        color: HexColor("#FFFFFC"),
-                                        elevation: 10,
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment: Alignment.topCenter,
-                                      child: Padding(
-                                        padding: EdgeInsets.only(
-                                            right: MediaQuery.of(context)
+                                        Align(
+                                          alignment: Alignment.topCenter,
+                                          child: Padding(
+                                            padding: EdgeInsets.only(
+                                                right: MediaQuery.of(context)
                                                     .size
                                                     .width *
-                                                0.4 /
-                                                11.0),
-                                        child: InkWell(
-                                          onTap: () async {
-                                            await showDialog(
-                                                context: context,
-                                                builder: (_) => ImageDialog());
-                                          },
-                                          child: CircleAvatar(
-                                            radius: circleRadius,
-                                            backgroundColor:
+                                                    0.4 /
+                                                    11.0),
+                                            child: InkWell(
+                                              onTap: () async {
+                                                await showDialog(
+                                                    context: context,
+                                                    builder: (_) => ImageDialog());
+                                              },
+                                              child: CircleAvatar(
+                                                radius: circleRadius,
+                                                backgroundColor:
                                                 HexColor("#002845"),
-                                            backgroundImage: NetworkImage(
-                                              "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+                                                backgroundImage: NetworkImage(
+                                                  "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+                                                ),
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                        );
-                      }),
+                            );
+                          }),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      )),
+          )),
     );
     ;
   }
