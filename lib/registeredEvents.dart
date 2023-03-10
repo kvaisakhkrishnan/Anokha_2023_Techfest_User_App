19df8f6db264f4d6a1951bb223ceca300905ae38
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-03-10 19:54:51 +0530

    Committed at Fri 10 Mar 7:54 PM

--- lib/registeredEvents.dart
+++ lib/registeredEvents.dart
@@ -1,4 +1,3 @@
-
 import 'package:anokha_home/registeredEventsCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -13,18 +12,15 @@ class RegisteredEvents extends StatefulWidget {
 }
 
 class _RegisteredEventsState extends State<RegisteredEvents> {
-
-
   final scrollController = ScrollController();
+  final searchController = TextEditingController();
 
-  void onListen(){
-
+  void onListen() {
     setState(() {});
   }
 
   @override
   void initState() {
-    // characters.allAll(List.from(characters));
     scrollController.addListener(onListen);
     super.initState();
   }
@@ -36,67 +32,93 @@ class _RegisteredEventsState extends State<RegisteredEvents> {
   }
 
 
+  void onSearch() {
+    // Perform search using the searchController.text value
+    print('Performing search for ${searchController.text}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-appBar: AppBar(elevation: 0, backgroundColor: Colors.white,
-toolbarHeight: 0.0,),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F5),
+               borderRadius: BorderRadius.circular(30.0)
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, top: 3, bottom: 3),
+                child: TextFormField(
+                  style: TextStyle(
+                    fontSize: 19
+                  ),
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search Card',
+                    border: InputBorder.none,
+
+                  ),
+                ),
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: IconButton(
+                iconSize: 30,
+                onPressed: onSearch,
+                icon: Icon(Icons.search,
+                color: Colors.black,),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.all(0.0),
           child: CustomScrollView(
             controller: scrollController,
             slivers: [
-
               SliverToBoxAdapter(
-                child:Container(
+                child: Container(
                   constraints: BoxConstraints.expand(
-                    height: MediaQuery.of(context).size.height * 0.13
-                  ),
+                      height: MediaQuery.of(context).size.height * 0.13),
                 ),
               ),
-
-
-
-              SliverList(delegate: SliverChildBuilderDelegate(
-                  (context, index){
-
-                  //final character = characters[index];
-
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                      (context, index) {
                     final itemPositionOffset = index * itemSize * 0.7;
-                    final difference = scrollController.offset - itemPositionOffset;
-                    if(index == 0) print("UFF ${difference }");
+                    final difference =
+                        scrollController.offset - itemPositionOffset;
+                    if (index == 0) print("UFF ${difference }");
                     final percent = 1.2 - difference / itemSize * 0.7;
                     double opacity = percent;
                     double scale = percent;
-                   // print(scale);
                     if (opacity >= 1.0) opacity = 1.0;
-                    if( opacity < 0.0) opacity = 0.0;
-
-                    if(percent >= 1.0) scale = 1.0;
-
-
-
-
+                    if (opacity < 0.0) opacity = 0.0;
+                    if (percent >= 1.0) scale = 1.0;
                     return Align(
                       heightFactor: 0.7,
-
                       child: Opacity(
                         opacity: opacity,
-
                         child: Transform(
                           alignment: Alignment.center,
                           transform: Matrix4.identity()..scale(scale, 1.0),
                           child: RegisteredEventCard(),
-
                         ),
                       ),
                     );
-
                   },
-                childCount: 10
-                  ),
+                  childCount: 10,
+                ),
               ),
             ],
           ),
@@ -105,4 +127,3 @@ toolbarHeight: 0.0,),
     );
   }
 }
-
