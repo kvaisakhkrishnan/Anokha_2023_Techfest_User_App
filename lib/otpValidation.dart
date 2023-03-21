859c671d4f72419562245142a4d2372b52cc819e
MITHUN-MENON-M <mithunmadhuchittur@gmail.com>
2023-03-21 11:32:21 +0530

    new commit otpValidation.dart

--- lib/otpValidation.dart
+++ lib/otpValidation.dart
@@ -23,125 +23,131 @@ class _OTPVerifyState extends State<OTPVerify> {
       appBar: AppBar(elevation: 0,
       toolbarHeight: 0,
       backgroundColor: Colors.white,),
-      body: Column(
+      body: SingleChildScrollView(
 
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 20.0, top: 20.0, bottom: 40.0),
-            child: Container(
-              alignment: Alignment.topLeft,
-              child: Text("VERIFY\nOTP",
-                style: GoogleFonts.montserrat(
-                  fontSize: 70.0,
-                  fontWeight: FontWeight.bold,
-                ),
+        child: IntrinsicHeight(
 
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Container(
-                child: Text("We have send the verification code to",
-                style: TextStyle(
-                  fontSize: 18.0
-                ),),
-            alignment: Alignment.topLeft,),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left : 20.0),
-            child: Container(
-              alignment: Alignment.topLeft,
-              child: Text("cb.en.u4cse*********",
-                style: TextStyle(
-                    fontSize: 18.0
+          child: Column(
+
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, top: 20.0, bottom: 40.0),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text("VERIFY\nOTP",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 70.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+
+                  ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 10.0),
-            child: Container(
-              alignment: Alignment.topLeft,
-              child: TextButton(
-                  onPressed: (){},
-                  child: Text("Change Email?",
+              Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Container(
+                    child: Text("We have send the verification code to",
                     style: TextStyle(
-                        fontSize: 18.0,
-                        color: Color(0xFFFF7F11)
+                      fontSize: 18.0
                     ),),
+                alignment: Alignment.topLeft,),
               ),
-            ),
-          ),
-
-
-
-      Padding(
-        padding: EdgeInsets.only(top: 40.0),
-        child: OtpTextField(
-         showFieldAsBox: true,
-          fieldWidth: 50.0,
-          focusedBorderColor: Color(0xFFFF7F11),
-          numberOfFields: 6,
-          filled: true,
-          fillColor: Color(0xffF3F2F7),
-          onSubmit: (value) {
-
-          },
-        ),
-      ),
+              Padding(
+                padding: const EdgeInsets.only(left : 20.0),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text("cb.en.u4cse*********",
+                    style: TextStyle(
+                        fontSize: 18.0
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: (){},
+                      child: Text("Change Email?",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFFFF7F11)
+                        ),),
+                  ),
+                ),
+              ),
 
 
 
           Padding(
             padding: EdgeInsets.only(top: 40.0),
-              child: Text("Time Remaining: ${timer} seconds")),
+            child: OtpTextField(
+             showFieldAsBox: true,
+              fieldWidth: 50.0,
+              focusedBorderColor: Color(0xFFFF7F11),
+              numberOfFields: 6,
+              filled: true,
+              fillColor: Color(0xffF3F2F7),
+              onSubmit: (value) {
 
-          Expanded(child: SizedBox()),
-          Padding(
-            padding: EdgeInsets.only(bottom: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton(
-                    onPressed: (){},
-                    child: Text("Resend OTP",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                          color: Color(0xFFFF7F11)
+              },
+            ),
+          ),
+
+
+
+              Padding(
+                padding: EdgeInsets.only(top: 40.0),
+                  child: Text("Time Remaining: ${timer} seconds")),
+
+              Expanded(child: SizedBox()),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                        onPressed: (){},
+                        child: Text("Resend OTP",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                              color: Color(0xFFFF7F11)
+                          ),
+                        ),
+                            ),
+
+
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical:
+                      30.0),
+                      child: OutlinedButton(
+                        onPressed: (){},
+                        child:
+                        Text("VERIFY",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0
+                          ),),
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Color(0xFFFF7F11),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                          padding: EdgeInsets.symmetric(vertical: 13.0, horizontal:
+                          MediaQuery.of(context).size.width * 0.15)
+
+
+                        ),
                       ),
                     ),
-                        ),
-
-
-
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical:
-                  30.0),
-                  child: OutlinedButton(
-                    onPressed: (){},
-                    child:
-                    Text("VERIFY",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0
-                      ),),
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Color(0xFFFF7F11),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                      padding: EdgeInsets.symmetric(vertical: 13.0, horizontal:
-                      MediaQuery.of(context).size.width * 0.15)
-
-
-                    ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          )
+              )
 
 
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
