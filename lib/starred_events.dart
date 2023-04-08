519ccde2c455a507d2e2f29989e3c20245e0e832
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-04-08 16:44:03 +0530

    hv

--- lib/starred_events.dart
+++ lib/starred_events.dart
@@ -10,7 +10,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class GetStarrs extends StatefulWidget {
-  const GetStarrs({Key? key}) : super(key: key);
+  var data = "";
+  GetStarrs({Key? key, required this.data}) : super(key: key);
 
   @override
   State<GetStarrs> createState() => _GetStarrsState();
@@ -20,10 +21,8 @@ class _GetStarrsState extends State<GetStarrs> {
   String url = "http://52.66.236.118:3060/userApp/getStarredEvents";
 
   Future<Map<String, dynamic>> getData() async {
-    var token =
-        "v4.public.eyJ1c2VyRW1haWwiOiJjYi5lbi51NGNzZTIwMDEwQGNiLnN0dWRlbnRzLmFtcml0YS5lZHUiLCJmdWxsTmFtZSI6IkZJUlNUTkFNRTAgTEFTVE5BTUUwIiwiY29sbGVnZU5hbWUiOiJBYXphZCBDb2xsZWdlIG9mIEVkdWNhdGlvbiAoSWQ6IEMtMzkyMzApIiwiZGlzdHJpY3QiOiJQcmFrYXNhbSIsImNvdW50cnkiOiJJTkRJQSIsInJvbGUiOiJVU0VSIiwic2VjcmV0X2tleSI6IkUpSEBNY1FmVGpXblpyNHU3eCFBJUQqRy1KYU5kUmdVa1hwMnM1djh5L0I_RShIK01iUGVTaFZtWXEzdDZ3OXokQyZGKUpATmNSZlRqV25acjR1N3ghQSVEKkctS2FQZFNnVmtYcDJzNXY4eS9CP0UoSCtNYlFlVGhXbVpxM3Q2dzl6JEMmRilKQE5jUmZValhuMnI1dTd4IUElRCpHLUthUGRTZ1ZrWXAzczZ2OXkkQj9FKEgrTWJRZVRoV21acTR0N3cheiVDKkYpSkBOY1JmVWpYbjJyNXU4eC9BP0QoRytLYVBkU2dWa1lwM3M2djl5JEImRSlIQE1jUWVUaFdtWnE0dDd3IXolQypGLUphTmRSZ1VqWG4ycjV1OHgvQT9EKEcrS2JQZVNoVm1ZcDNzNnY5eSRCJkUpSEBNY1FmVGpXblpyNHQ3dyF6JUMqRi1KYU5kUmdVa1hwMnM1djh4L0E_RChHK0tiUGVTaFZtWXEzdDZ3OXokQiZFKUhATWNRZlRqV25acjR1N3ghQSVEKkYtSmFOZFJnVWtYcDJzNXY4eS9CP0UoSCtLYlBlU2hWbVlxM3Q2dzl6JEMmRilKQE5jUWZUalduWnI0dTd4IUElRCpHLUthUGRTZ1VrWHAyczV2OHkvQj9FKEgrTWJRZVRoV21ZcTN0Nnc5eiRDJEImRShIK01iUWVUaFdtWnE0dDd3IXolQypGLUpATmNSZlVqWG4ycjV1OHgvQT9EKEcrS2JQZFNnVmtZcDNzNnY5eSRCJkUpSEBNY1FmVGhXbVpxNHQ3dyF6JUMqRi1KYU5kUmdVa1huMnI1dTh4L0E_RChHK0tiUGVTaFZtWXEzczZ2OXkkQiZFKUhATWNRZlRqV25acjR1N3cheiVDKkYtSmFOZFJnVWtYcDJzNXY4eS9BP0QoRytLYlBlU2hWbVlxM3Q2dzl6JEMmRSlIQE1jUWZUalduWnI0dTd4IUElRCpHLUphTmRSZ1VrWHAyczV2OHkvQj9FKEgrTWJQZVNoVm1ZcTN0Nnc5eiRDJkYpSkBOY1JmVGpXblpyNHU3eCFBJUQqRy1LYVBkU2dWa1lwMnM1djh5L0I_RShIK01iUWVUaFdtWnE0dDZ3OXokQyZGKUpATmNSZlVqWG4ycjV1OHghQSVEKkctS2FQZFNnVmtZcDNzNnY5eSRCP0UoSCtNYlFlVGhXbVpxNHQ3dyF6JUMqRilKQE5jUmZValhuMnI1dTh4L0E_RChHK0thUGRTZ1ZrWXAzczZ2OXkkQiZFKUhATWNRZVRoV21acTR0N3cheiVDKkYtSmFOZFJnVWpYbjJyNXU4eC9BP0QoRytLYlBlU2hWbVlwM3M2djl2OHkvQj9FKEcrS2JQZVNoVm1ZcTN0Nnc5eiRDJkYpSkBNY1FmVGpXblpyNHU3eCFBJUQqRy1LYVBkUmdVa1hwMnM1djh5L0I_RShIK01iUWVUaFZtWXEzdDZ3OXokQyZGKUpATmNSZlVqWG5acjR1N3ghQSVEKkctS2FQZFNnVmtZcDNzNXY4eS9CP0UoSCtNYlFlVGhXbVpxNHQ3dzl6JEMmRilKQE5jUmZValhuMnI1dTh4L0ElRCpHLUthUGRTZ1ZrWXAzczZ2OXkkQiZFKEgrTWJRZVRoV21acTR0N3cheiVDKkYtSkBOY1JmVWpYbjJyNXU4eC9BP0QoRytLYlBkU2dWa1lwM3M2djl5JEImRSlIQE1jUWZUaFdtWnE0dDd3IXolQypGLUphTmRSZ1VrWG4ycjV1OHgvQT9EKEcrS2JQZVNoVm1ZcTNzNnY5eSRCJkUpSEBNY1FmVGpXblpyNHU3dyF6JUMqRi1KYU5kUmdVa1hwMnM1djh5L0E_RChHK0tiUGVTaFZtWXEzdDZ3OXokQyZFKUhATWNRZlRqV25acjR1N3ghQSVEKkctS2FOZFJnVWtYcDJzNXY4eS9CP0UoSCtNYlFlU2hWbVlxM3Q2dzl6JEMmRilKQE5jUmZValduWnI0dTd4IUElRCpHLUthUGRTZ1ZrWXAyczJyNXU4eC9BP0QoRy1LYVBkU2dWa1lwM3M2djl5JEImRSlIQE1iUWVUaFdtWnE0dDd3IXolQypGLUphTmRSZlVqWG4ycjV1OHgvQT9EKEcrS2JQZVNoVmtZcDNzNnY5eSRCJkUpSEBNY1FmVGpXblpxNHQ3dyF6JUMqRi1KYU5kUmdVa1hwMnM1dTh4L0E_RChHK0tiUGVTaFZtWXEzdDZ3OXkkQiZFKUhATWNRZlRqV25acjR1N3ghQSVDKkYtSmFOZFJnVWtYcDJzNXY4eS9CP0UoRytLYlBlU2hWbVlxM3Q2dzl6JEMmRilKQE1jUWZUalduWnI0dTd4IUElRCpHLUthUGRSZ1VrWHAyczV2OHkvQj9FKEgrTWJRZVRoVm1ZcTN0Nnc5eiRDJkYpSkBOY1JmVWpYblpyNHU3eCFBJUQqRy1LYVBkU2dWa1lwM3M1djh5L0I_RShIK01iUWVUaFdtWnE0dDd3OXokQyZGKUpATmNSZlVqWG4ycjV1OHgvQSVEKkctS2FQZFNnVmtZcDNzNnY5eSRCJkUoSCtNYlFlVGhXbVpxNHQ3dyF6JUMqRi1KQE5jUmZValhuMnI1dTh4L0E_RChHK0tiUGRTZ1ZrWXAzczZ2OXkkQiZFKUhATWNRZlRoV21acTR0N3cheiVDKkYtSmFOZFJnVWtYaXdoamRqd2lkanFzcW93YmR4aGpXblpyNHU3eCFBJUQqRi1KYU5kUmdVa1hwMnM1djh5L0I_RShIK0tiUGVTaFZtWXEzdDZ3OXokQyZGKUpATmNRZlRqV25acjR1N3ghQSVEKkctS2FQZFNnVWtYcDJzNXY4eS9CP0UoSCtNYlFlVGhXbVlxM3Q2dzl6JEMmRilKQE5jUmZValhuMnI1dTd4IUElRCpHLUthUGRTZ1ZrWXAzczZ2OXkvQj9FKEgrTWJRZVRoV21acTR0N3cheiVDJkYpSkBOY1JmVWpYbjJyNXU4eC9BP0QoRy1LYVBkU2dWa1lwM3M2djl5JEImRSlIQE1iUWVUaFdtWnE0dDd3IXolQypGLUphTmRSZlVqWG4ycjV1OHgvQT9EKEcrS2JQZVNoVmtZcDNzNnY5eSRCJkUpSEBNY1FmVGpXblpxNHQ3dyF6JUMqRi1KYU5kUmdVa1hwMnM1dTh4L0E_RChHK0tiUGVTaFZtWXEzdDZ3OXkkQiZFKUhATWNRZlRqV25acjR1N3ghQSVDKkYtSmFOZFJnVWtYcDJzNXY4dyIsImlhdCI6IjIwMjMtMDQtMDhUMDk6MjQ6MjMuMjA3WiIsImV4cCI6IjIwMjMtMDQtMDhUMTA6MjQ6MjMuMjA3WiJ9EPvKHdoGlp6892xZeidfw3-lrRE0GPmLeq39Se_AMaR4-VqQz5Fgs1Bm8UUB3ii4GKWX85gXUBN7ZNre4lEhBg";
-    final response = await http
-        .get(Uri.parse(url), headers: {'authorization': 'Bearer $token'});
+    final response = await http.get(Uri.parse(url),
+        headers: {'authorization': 'Bearer ${widget.data}'});
     return json.decode(response.body);
   }
 
@@ -85,8 +84,7 @@ class _WheelState extends State<Wheel> {
                           context,
                           MaterialPageRoute(
                               builder: (context) => EventInfo(
-                                  event_map: widget
-                                      .starr_map?[index])));
+                                  event_map: widget.starr_map?[index])));
                     },
                   ),
                 );
