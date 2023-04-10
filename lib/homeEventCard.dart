59b498dd6df47fd0322c1d838aec38d279e5bbf9
MITHUN-MENON-M <mithunmadhuchittur@gmail.com>
2023-04-10 16:35:06 +0530

    new commit

--- lib/homeEventCard.dart
+++ lib/homeEventCard.dart
@@ -15,7 +15,6 @@ class EventCard extends StatefulWidget {
   var data;
   EventCard({Key? key, required this.event_data, required this.data}) : super(key: key);
 
-
   @override
   State<EventCard> createState() => _EventCardState();
 }
@@ -68,132 +67,100 @@ class _EventCardState extends State<EventCard> {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Padding(
-        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        child: InkWell(
-          onTap: (){
-
-            Navigator.push(context, MaterialPageRoute(builder: (context) => EventInfo(event_map: widget.event_data, data:widget.data)));
-
-          },
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      EventInfo(event_map: widget.event_data, data: widget.data)));
+        },
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
                   boxShadow: [
-              BoxShadow(
-              color: Colors.grey,
-              spreadRadius: 1.0,
-              blurRadius: 10.0
-          )
-            ],
-                  image: DecorationImage(
-                    image: NetworkImage(widget.event_data.url),
-                    fit: BoxFit.fill
-                  ),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(25.0), topRight: Radius.circular(25.0))
-                ),
-                constraints: BoxConstraints.expand(
-                  height: MediaQuery.of(context).size.width * 0.4 * 1.0,
-                  width: MediaQuery.of(context).size.width * 0.4
-                ),
-
-
-              ),
-              Container(
-
-                child:
-              Row(
-
-                children: [
-
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(widget.event_data.name,
-
-                        style: TextStyle(
-                          color: Color(0xFF002845),
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w500,
-                        ),),
-                        Text(widget.event_data.type == 0 ? "Event" : "Workshop" ,
-                          style: TextStyle(
-                          color: Color(0xFFFF7F11)
-                          ),)
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-
-                    ),
-                  ),
-
-                  Expanded(
-                    child: widget.event_data.isStarred == 0 ? IconButton(
-                        iconSize: 30.0,
-                        alignment: Alignment.centerRight,
-                        onPressed: (){
-                          setState(() {
-                            widget.event_data.isStarred = 1;
-                          });
-                          if(widget.event_data.isStarred == 1)
-                            {
-                              String url =  __url + "userApp/insertStarrs";
-                              addStarred(url);
-                            }
-
-
-                        },
-                        icon: Icon(
-                            Icons.star_border,
-                        ))
-                    :
-                    IconButton(
-                        iconSize: 30.0,
-                        color: Color(0xFFFF7F11),
-                        alignment: Alignment.centerRight,
-                        onPressed: (){
-                          setState(() {
-                            widget.event_data.isStarred  = 0;
-                            if(widget.event_data.isStarred == 0)
-                            {
-                              String url =  __url + "userApp/dropStarrs";
-                              removeStarred(url);
-                            }
-                          });
-                        },
-                        icon: Icon(
-                            Icons.star
-                        )),
-                  )
-                ],
-              ),
-                constraints: BoxConstraints.expand(
-                    height: MediaQuery.of(context).size.width * 0.4 * 0.4,
-                    width: MediaQuery.of(context).size.width * 0.4
-                ),
-                decoration: (
-                BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 1.0,
-                      blurRadius: 10.0
-                    )
+                    BoxShadow(color: Colors.grey, spreadRadius: 1.0, blurRadius: 10.0)
                   ],
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25.0), bottomRight: Radius.circular(25.0))
-                )
+                  image: DecorationImage(
+                      image: NetworkImage(widget.event_data.url), fit: BoxFit.fill),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25.0))),
+              constraints: BoxConstraints.expand(
+                  height: MediaQuery.of(context).size.width * 0.4 * 1.0,
+                  width: MediaQuery.of(context).size.width * 0.42),
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0,top: MediaQuery.of(context).size.height * 0.02),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.event_data.name,
+                      style: TextStyle(
+                        color: Color(0xFF002845),
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    // SizedBox(height: MediaQuery.of(context).size.height * 0.001),
+                    Row(
+                      children: [
+                        Text(
+                          widget.event_data.type == 0 ? "Event" : "Workshop",
+                          style: TextStyle(color: Color(0xFFFF7F11),fontSize: 17.0,),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          iconSize: 30.0,
+                          color: widget.event_data.isStarred == 1
+                              ? Color(0xFFFF7F11)
+                              : null,
+                          onPressed: () {
+                            setState(() {
+                              widget.event_data.isStarred =
+                                  1 - widget.event_data.isStarred;
+                              String url = __url +
+                                  (widget.event_data.isStarred == 1
+                                      ? "userApp/insertStarrs"
+                                      : "userApp/dropStarrs");
+                              if (widget.event_data.isStarred == 1) {
+                                addStarred(url);
+                              } else {
+                                removeStarred(url);
+                              }
+                            });
+                          },
+                          icon: Icon(Icons.star),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-
-                )
-
-            ],
-
-    ),
+              ),
+              constraints: BoxConstraints.expand(
+                  height: MediaQuery.of(context).size.width * 0.4 * 0.6,
+                  width: MediaQuery.of(context).size.width * 0.42),
+              decoration: (BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey, spreadRadius: 1.0, blurRadius: 10.0)
+                ],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25.0),
+                    bottomRight: Radius.circular(25.0)),
+              )),
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
